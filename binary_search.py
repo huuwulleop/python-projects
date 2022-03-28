1f8fc76c48e6161207b02f594014db057149e687
Vuong Phan <huuwulleop@hotmail.com>
2022-03-28 06:47:08 -0400

    Fix binary search bug

--- binary_search.py
+++ binary_search.py
@@ -1,26 +1,26 @@
 # Find a number in ordered list using binary search
 
-def binary_search(arr, num):
-    low = 0
-    high = len(arr) - 1
+def binary_search(arr, num, low=None, high=None):
+    if low is None:
+        low = 0
+    if high is None:
+        high = len(arr) - 1
 
     mid = (low + high) // 2
-    print(mid)
+
+    if high < low:
+        return -1
 
     if arr[mid] == num:
         return mid
     elif num < arr[mid]:
-        print(arr[:mid])
-        return binary_search(arr[:mid], num)
+        return binary_search(arr, num, low, mid - 1)
     else:
         # num > arr[mid]
-        print(arr[(mid + 1):])
-        return binary_search(arr[mid:], num)
+        return binary_search(arr, num, mid + 1, high)
 
 def main():
     arr = [1, 3, 5, 9, 14, 22, 36]
-    arr2 = [14, 22, 36]
-    #print(len(arr2) // 2)
     print(binary_search(arr, 14))
 
 main()
